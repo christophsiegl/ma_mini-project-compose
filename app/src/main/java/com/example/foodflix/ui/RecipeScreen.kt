03c574e5d7b5b82ffc183ffbcc4675fc0c977905
Christoph Siegl <csiegl@student.tugraz.at>
2023-05-04 15:18:05 +0200

    Created all screens and implemented basic navigation to some of them.

---
+++ app/src/main/java/com/example/foodflix/ui/RecipeScreen.kt
@@ -0,0 +1,30 @@
+package com.example.foodflix.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RecipeScreen(
+    onToLoginScreenButtonClicked: () -> Unit = {},
+    modifier: Modifier = Modifier
+){
+
+    Column() {
+        Text(text = "Recipe Screen")
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(
+            onClick = onToLoginScreenButtonClicked,
+            modifier = modifier.widthIn(min = 250.dp)
+        ) {
+            Text("To Login Screen")
+        }
+    }
+}
